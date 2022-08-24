4e93e1b092357c9ea98ffd6e0cb1bfa3a3adbf08
Anna Kovalchuk <annazukh@gmail.com>
2022-08-24 22:48:19 +0300

    markup-rendering

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//створюємо та додаємо розмітку
+gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+function createMarkup(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  });
+  return markup.join("");
+}
+
+gallery.addEventListener("click", (ev) => {});
