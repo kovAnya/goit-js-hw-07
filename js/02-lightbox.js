a6bf93d92e39cb91756b6dcb72c33387d584e9fe
Anna Kovalchuk <annazukh@gmail.com>
2022-08-25 01:25:35 +0300

    task-2_completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+gallery.addEventListener("click", onClick);
+
+function createMarkup(galleryItems) {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+  <div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </div>`;
+  });
+  return markup.join("");
+}
+
+function onClick(event) {
+  event.preventDefault();
+  const lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
